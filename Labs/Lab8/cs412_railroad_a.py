01effff4e700059bc0faed898699a88f3ffda3e9
CamHak <camhakenson@gmail.com>
2025-10-16 19:33:07 -0400

    more

--- Labs/Lab8/cs412_railroad_a.py
+++ Labs/Lab8/cs412_railroad_a.py
@@ -12,7 +12,7 @@ import math
 
 # calculates distance between two points
 def calc_distance(points1, points2):
-    return round(math.dist(points1, points2), 1)
+    return math.dist(points1, points2)
 
 
 # creates the graph using adj format
@@ -30,10 +30,46 @@ def make_adj(points):
 
 
 def boruvka(graph):
-    forest = {}
-    count, labels = count_and_label(graph)
+    forest = {i: {} for i in graph}
+    count, labels = count_and_label(forest)
+    seen_edges = set()
+    total = 0
+
+    # gathers all the edges
+    edges = []
+    for u in graph:
+        for v, w in graph[u].items():
+            if u < v:
+                edges.append((u, v, w))
+
     while count > 1:
-        pass
+        safe = {}
+
+        for u, v, w in edges:
+            if labels[u] != labels[v]:
+                if labels[u] not in safe or w < safe[labels[u]][2]:
+                    safe[labels[u]] = (u, v, w)
+                if labels[v] not in safe or w < safe[labels[v]][2]:
+                    safe[labels[v]] = (u, v, w)
+
+        for u, v, w in safe.values():
+            edge = (min(u, v), max(u, v))
+            if edge in seen_edges:
+                continue
+            forest[u][v] = w
+            forest[v][u] = w
+            seen_edges.add(edge)
+            total += w
+
+        count, labels = count_and_label(forest)
+
+    return total
+
+
+def calc_cost(mst):
+    for value in mst.values():
+        for v in value:
+            print(v)
 
 
 def main():
@@ -47,6 +83,8 @@ def main():
         count += 1
 
     graph = make_adj(points)
+    cost = boruvka(graph)
+    print(f"${cost:.1f}M")
 
 
 if __name__ == "__main__":
