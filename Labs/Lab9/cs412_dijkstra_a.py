e9269981fb3520902cae80100fbca5bb52b5c542
Cam-Hak <camhakenson@gmail.com>
2025-11-04 18:25:48 -0500

    finished lab

--- Labs/Lab9/cs412_dijkstra_a.py
+++ Labs/Lab9/cs412_dijkstra_a.py
@@ -19,14 +19,14 @@ def dijkstras(n, edges, src):
     pq.put((0, src))
 
     while not pq.empty():
-        edge, weight = pq.get()
+        weight, edge = pq.get()
         if edge in shortest:
             continue
 
         shortest[edge] = weight
         for e, w in adj[edge]:
             if e not in shortest:
-                pq.put((w, e))
+                pq.put((w + weight, e))
 
     return shortest
 
@@ -56,7 +56,17 @@ def main():
 
         querys.append(query)
 
-    print(dijkstras(n, edges, 0))
+    # used for testing
+    # n = 4
+    # edges = [[0, 1, 2], [1, 2, 2], [3, 0, 2]]
+    # querys = [[0, 0], [0, 1], [0, 2], [0, 3]]
+
+    for s, e in querys:
+        path = dijkstras(n, edges, s)
+        if e in path:
+            print(path[e])
+        else:
+            print("Impossible")
 
 
 if __name__ == "__main__":
